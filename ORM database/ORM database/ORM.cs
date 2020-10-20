7eadfe04fd1b6a0cc37274654866c6853b3dc3d6
Mads <madsi2000@gmail.com>
2020-10-20 07:11:24 +0200

    update

--- ORM database/ORM database/ORM.cs
+++ ORM database/ORM database/ORM.cs
@@ -6,14 +6,17 @@ namespace ORM_database
 {
     abstract class ORMField
     {
-        public int name { get; set; }
+        public string Name { get; set; }
         public abstract string GetSQLValue(ORM orm);
-            
+        public ORMField(string name)
+        {
+            Name = name;
+        }
     }
     class ORMInt : ORMField
     {
         Func<ORM, int> Getter { get; set; }
-        public ORMInt(Func<ORM, int> getter)
+        public ORMInt(string name, Func<ORM, int> getter) : base(name)
         {
             Getter = getter;
         }
@@ -22,62 +25,115 @@ namespace ORM_database
             return Getter(orm).ToString();
         }
     }
-    class ORMString
+    class ORMString : ORMField
     {
-
-        public string field { get; set; }
+        Func<ORM, string> Getter { get; set; }
+        public ORMString(string name, Func<ORM, string> getter) : base (name)
+        {
+            Getter = getter;
+        }
+        public override string GetSQLValue(ORM orm)
+        {
+            return "'"+Getter(orm)+"'";
+        }
     }
     abstract class ORM
     {
         protected abstract string TableName();
         static Dictionary<string, Dictionary<string, ORMField>> tables = new Dictionary<string, Dictionary<string, ORMField>>();
-
+        static Dictionary<string, ORMField> primary_keys = new Dictionary<string, ORMField>();
         static ORM()
         {
-            Console.WriteLine("Hej orm");
+            //Database connection
         }
-        protected static void Int(string table, string field, Func<ORM, int> getter)
+        protected static void Add(string table, ORMField field)
         {
             if (tables.ContainsKey(table) == false)
             {
                 tables[table] = new Dictionary<string, ORMField>();
             }
-            tables[table][field] = new ORMInt(getter);
+            tables[table][field.Name] = field;
+        }
+        protected static void Int(string table, string field, Func<ORM, int> getter)
+        {
+            Add(table, new ORMInt(field, getter));
         }
         protected static void String(string table, string field, Func<ORM, string> getter)
         {
+            Add(table, new ORMString(field, getter));
 
+        }
+        protected static void PrimaryKey(string table, string fieldName)
+        {
+            primary_keys[table] = tables[table][fieldName];
         }
         public void Insert()
         {
             string tableName = TableName();
             string columns;
-            string values = "";
-            List<string> fieldValues = new List<string>();
+            string values;
+            string fieldName;
             List<string> fieldNames = new List<string>();
-            foreach(KeyValuePair<string, ORMField> kv in tables[tableName])
+            List<string> fieldValues = new List<string>();
+            foreach (KeyValuePair<string, ORMField> kv in tables[tableName])
             {
-                string fieldName = kv.Key;
+                if(primary_keys[tableName] == kv.Value)
+                {
+                    continue;
+                }
+                fieldName = kv.Key;
                 fieldNames.Add(fieldName);
-                fieldValues.Add(ORMInt.GetSQLValue())
+                fieldValues.Add(kv.Value.GetSQLValue(this));
             }
 
-            values = string.Join(',', fieldValues);
             columns = string.Join(',', fieldNames);
+            values = string.Join(',', fieldValues);
             string sql = $"INSERT INTO ´{tableName}´ ({columns}) VALUES ({values})";
             Console.WriteLine(sql);
         }
         public void Update()
         {
+            string tableName = TableName();
+            string sets;
+            string primaryKey = PKName();
+            string primaryKeyValue = PKValue();
+            string fieldName;
+            string fieldValue;
+            List<string> setList = new List<string>();
 
+            foreach (KeyValuePair<string, ORMField> kv in tables[tableName])
+            {
+                if (primary_keys[tableName] == kv.Value)
+                {
+                    continue;
+                }
+                fieldName = kv.Key;
+                fieldValue = kv.Value.GetSQLValue(this);
+                setList.Add($"{fieldName}={fieldValue}");
+            }
+            sets = string.Join(',', setList);
+            string sql = $"UPDATE {tableName} SET {sets} WHERE {primaryKey}={primaryKeyValue}";
+            Console.WriteLine(sql);
         }
         public void Delete()
         {
-
+            string tableName = TableName();
+            string primaryKey = PKName();
+            string primaryKeyValue = PKValue();
+            string sql = $"DELETE FROM {tableName} WHERE {primaryKey}={primaryKeyValue}";
+            Console.WriteLine(sql);
         }
         public void Select(string where)
         {
 
+        }
+        public string PKName()
+        {
+            return primary_keys[TableName()].Name;
+        }
+        public string PKValue()
+        {
+            return primary_keys[TableName()].GetSQLValue(this);
         }
     }
 }
--- ORM database/ORM database/Program.cs
+++ ORM database/ORM database/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Security;
 
 namespace ORM_database
 {
-
-
     class Program
     {
         static void Main(string[] args)
         {
             Item item = new Item();
+            item.ItemId = 101;
+            item.Title = "Stol";
+            item.Description = "Noget man sidde på";
             item.Insert();
+            item.Title = "Ægget";
+            item.Update();
+            item.Delete();
         }
     }
     class Item : ORM
@@ -19,6 +24,7 @@ namespace ORM_database
         public const string TABLE_NAME = "items";
         public int ItemId { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         protected override string TableName()
         {
             return TABLE_NAME;
@@ -30,8 +36,9 @@ namespace ORM_database
         static Item()
         {
             Int(TABLE_NAME, "itemId", (orm) => (orm as Item).ItemId);
-            String(TABLE_NAME, "itemId", (orm) => (orm as Item).Title);
+            String(TABLE_NAME, "Navn", (orm) => (orm as Item).Title);
+            String(TABLE_NAME, "description", (orm) => (orm as Item).Description);
+            PrimaryKey(TABLE_NAME, "itemId");
         }
-    
     }
 }
